033ccb24a8ba827b4c4d081b38de79b57a5e630c
Ridge <ridgechristensen28@mail.com>
2022-11-23 15:52:13 -0700

    update1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(`${__dirname}`))
+app.use(express.static(`${__dirname}/public`))
 
 app.listen(4000,
     () => console.log(`server running on 4000`)
